b5bcad9868f1df584514b47957ef3a5de12186b4
jihye <jiji.pak41@gmail.com>
2021-10-27 12:54:03 +0100

    fetch API
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

--- Guessing Game/src/App.jsx
+++ Guessing Game/src/App.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [user, setUser] = React.useState('');
+  // const [data,getData]= React.uuseState('Alisyad')
+
+  const url = 'https://api.github.com/users/';
+
+  React.useEffect(() => {
+    fetch(url + 'jijip41')
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  console.log(user);
+  // if(!user) return <div>Loading...</div>
 
   return (
     <section>
       <header>
         <form>
+          <p>{user.name}</p>
           <label>User name</label>
           <input />
         </form>
