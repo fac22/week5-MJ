5a8f883e8b586f2b6c22dab24ff22296cb426dd2
jihye <jiji.pak41@gmail.com>
2021-10-27 15:21:03 +0100

    fetch followers data
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

--- Guessing Game/src/App.jsx
+++ Guessing Game/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 
+{
+  /* <img src={user.avatar_url} alt="" width="128" height="128" /> */
+}
+{
+  /* <ReposList url={user.repos_url} /> */
+}
 function App() {
   const [user, setUser] = React.useState('');
-  // const [data,getData]= React.uuseState('Alisyad')
+  const [followers, setFollowers] = React.useState('');
+  // const [data, getData] = React.uuseState('Alisyad');
 
   const url = 'https://api.github.com/users/';
 
@@ -15,6 +22,16 @@ function App() {
 
   console.log(user);
   // if(!user) return <div>Loading...</div>
+
+  React.useEffect(() => {
+    fetch(url + 'jijip41/followers')
+      .then((res) => res.json())
+      .then((data) => setFollowers(data));
+  }, []);
+
+  // const followers = url + 'jijip41/followers';
+  console.log({ followers });
+  console.log({ user });
 
   return (
     <section>
@@ -32,7 +49,15 @@ function App() {
       </header>
       <main>
         <div>
-          <img></img>
+          {followers.map((follower) => (
+            <img
+              src={follower.avatar_url}
+              alt=""
+              width="128"
+              height="128"
+              key={follower.id}
+            />
+          ))}
         </div>
         <div>
           <form>
