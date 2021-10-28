46595675af8b44bcf73e92d1545a76c0a51c0631
jihye <jiji.pak41@gmail.com>
2021-10-28 10:53:20 +0100

    create dashboard.jsx, make setinterval work
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

---
+++ GuessingGame/src/Dashboard.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Timer() {
+  const [time, setTime] = React.useState(60);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      if (time >= 1) {
+        setTime(time - 1);
+      } else if (time === 0) {
+        setTime('Time Over');
+      } else {
+        setTime('Time Over');
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [time]);
+
+  return (
+    <div>
+      <p>Time</p>
+      <div>{time}</div>
+    </div>
+  );
+}
+
+const followersImgs = document.querySelectorAll('.follower-img');
+console.log(followersImgs);
+
+function Score(props) {
+  const [score, setScore] = React.useState(0);
+  const [followers, setFollowers] = React.useState('');
+
+  return (
+    <div>
+      <p>Points</p>
+      <div>{score}</div>
+    </div>
+  );
+}
+
+export { Timer, Score };
