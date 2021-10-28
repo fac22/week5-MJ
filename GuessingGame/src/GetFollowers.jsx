4a045e7a74a98be737c0564db164e78f7991b0ba
jihye <jiji.pak41@gmail.com>
2021-10-28 10:51:48 +0100

    refactor useeffect, filter followers
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

---
+++ GuessingGame/src/GetFollowers.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import infos from './data';
+const url = 'https://api.github.com/users/';
+
+export default function GetFollowers(props) {
+  const [followers, setFollowers] = React.useState('');
+
+  React.useEffect(() => {
+    fetch(url + props.name + '/followers')
+      .then((res) => res.json())
+      .then((data) => setFollowers(data));
+  });
+
+  if (!followers) {
+    return <div>Loading...</div>;
+  } else if (!props.guess) {
+    return (
+      <div>
+        {followers.map((follower) => (
+          <img
+            src={follower.avatar_url}
+            alt=""
+            width="128"
+            height="128"
+            className="follower-img"
+            key={follower.id}
+            id={follower.id + ''}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {followers
+          .filter((follower) => follower.login != props.guess)
+          .map((follower) => (
+            <img
+              src={follower.avatar_url}
+              alt=""
+              width="128"
+              height="128"
+              className="follower-img"
+              key={follower.id}
+              id={follower.id + ''}
+            />
+          ))}
+      </div>
+    );
+  }
+}
