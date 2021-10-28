1ed365d5942f42c8ecff61d711b71a3c0e6625bb
jihye <jiji.pak41@gmail.com>
2021-10-28 10:52:14 +0100

    refactor useeffect
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

---
+++ GuessingGame/src/GetUser.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+const url = 'https://api.github.com/users/';
+
+export default function GetUser(props) {
+  const [userName, setUserName] = React.useState('jijip41');
+
+  React.useEffect(() => {
+    fetch(url + props.name)
+      .then((res) => res.json())
+      .then((data) => setUserName(data));
+  });
+
+  if (!userName) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h1>
+          <img src={userName.avatar_url} alt="" width="30" height="30" />
+          {userName.login}
+        </h1>
+      </div>
+    );
+  }
+}
